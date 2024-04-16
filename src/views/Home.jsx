78c3c5ff3adffa9f2dc1c23013fc38d7d34d153b
Linsiji <42350425+linsijiLaGuara@users.noreply.github.com>
2024-04-16 19:07:58 -0400

    add: nuevo doc

---
+++ src/views/Home.jsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="mt-5 centertext">
+      <h1>Bienvenido </h1>
+     
+    </div>
+  );
+}
---
+++ src/views/cv.jsx
@@ -0,0 +1,8 @@
+export default function CV() {
+    return (
+      <div className="mt-5 centertext">
+        <h1>mi cv</h1>
+       
+      </div>
+    );
+  }
